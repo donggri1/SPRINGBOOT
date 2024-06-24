9173240e718c98cce4b70d41c6ab87f5e29d8448
DCHA\diabl <diabl456@naver.com>
2024-06-25 00:53:47 +0900

    first

---
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,51 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member =new Member();
+        member.setName("spring");
+
+        memberRepository.save(member);
+
+        Member result = memberRepository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");;
+        memberRepository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");;
+        memberRepository.save(member2);
+
+        Member result = memberRepository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+    }
+
+
+}
